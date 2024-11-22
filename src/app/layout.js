6c167788f509b84206733eac4290b2374bdf1bd6
Suresh krishna <sureshkrishna@Sureshs-Laptop.local>
2024-11-22 23:14:42 +0530

    edit user ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={commissioner.className}>
         <div className="bg-[#F4F7FF] w-[100%] h-[100%]">
           <App>{children}</App>
