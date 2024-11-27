c85016bf5e179e7565badf71f3a9848e47a87682
jeevanantham123 <jeewa.mad.2000@gmail.com>
2024-11-27 19:17:48 +0530

    added download setup

--- src/app/auditing/master-data/page.js
+++ src/app/auditing/master-data/page.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import MasterData from './master-data';
+import MasterData from "./master-data-table";
 
 const MasterDataInfoPage = () => {
-    return (
-        <div>
-            <MasterData />
-        </div>
-    );
+  return (
+    <div className="container px-0 min-h-screen pt-20 mx-auto">
+      <MasterData />
+    </div>
+  );
 };
 
 export default MasterDataInfoPage;
