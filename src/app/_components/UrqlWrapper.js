0d99014a66a31a14b863cf39dec7480d245940cb
jeevanantham123 <jeewa.mad.2000@gmail.com>
2024-12-14 20:51:52 +0530

    graphql url changes

--- src/app/_components/UrqlWrapper.js
+++ src/app/_components/UrqlWrapper.js
@@ -1,15 +1,21 @@
-'use client';
+"use client";
 
-import { useMemo } from 'react';
-import { UrqlProvider, ssrExchange, cacheExchange, fetchExchange, createClient } from '@urql/next';
+import { useMemo } from "react";
+import {
+  UrqlProvider,
+  ssrExchange,
+  cacheExchange,
+  fetchExchange,
+  createClient,
+} from "@urql/next";
 
 export default function UrqlWrapper({ children }) {
   const [client, ssr] = useMemo(() => {
     const ssr = ssrExchange({
-      isClient: typeof window !== 'undefined',
+      isClient: typeof window !== "undefined",
     });
     const client = createClient({
-      url: 'http://207.180.215.123:8002/graphql/',
+      url: process.env.NEXT_PUBLIC_GRAPHQL_URL,
       exchanges: [cacheExchange, ssr, fetchExchange],
     });
 
